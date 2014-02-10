5e54a2ed0d67f72e4fbe4b4d09d17a977d23e9ac
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-02-10 18:11:43 +0100

    First Java API draft

---
+++ javawrapper/src/main/java/org/reactivecouchbase/japi/CouchbaseBucket.java
@@ -0,0 +1,164 @@
+package org.reactivecouchbase.japi;
+
+import com.couchbase.client.protocol.views.Query;
+import com.couchbase.client.protocol.views.View;
+import net.spy.memcached.PersistTo;
+import net.spy.memcached.ReplicateTo;
+import net.spy.memcached.ops.OperationStatus;
+import org.reactivecouchbase.Couchbase$;
+import org.reactivecouchbase.FutureHelper$;
+import org.reactivecouchbase.ScalaHelper$;
+import org.reactivecouchbase.client.Row;
+import org.reactivecouchbase.common.Functionnal;
+import org.reactivecouchbase.japi.concurrent.Future;
+import org.reactivecouchbase.json.Json;
+import org.reactivecouchbase.json.Reader;
+import org.reactivecouchbase.json.Writer;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.ExecutionContext$;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CouchbaseBucket {
+
+    private final org.reactivecouchbase.CouchbaseBucket bucket;
+
+    private final Integer expirationMillis;
+    private final PersistTo persistTo;
+    private final ReplicateTo replicateTo;
+    private final ExecutorService es;
+    private final ExecutionContext ec;
+    private final Couchbase$ couchbase;
+
+    public CouchbaseBucket(org.reactivecouchbase.CouchbaseBucket currentBucket) {
+        this.bucket = currentBucket;
+        this.expirationMillis = -1;
+        this.persistTo = PersistTo.ZERO;
+        this.replicateTo = ReplicateTo.ZERO;
+        this.es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        this.ec = ExecutionContext$.MODULE$.fromExecutorService(es);
+        this.couchbase = Couchbase$.MODULE$;
+    }
+
+    public CouchbaseBucket(org.reactivecouchbase.CouchbaseBucket currentBucket, ExecutorService ec) {
+        this.bucket = currentBucket;
+        this.expirationMillis = -1;
+        this.persistTo = PersistTo.ZERO;
+        this.replicateTo = ReplicateTo.ZERO;
+        this.es = ec;
+        this.ec = ExecutionContext$.MODULE$.fromExecutorService(es);
+        this.couchbase = Couchbase$.MODULE$;
+    }
+
+    public CouchbaseBucket(org.reactivecouchbase.CouchbaseBucket currentBucket, Integer expirationMillis, PersistTo persistTo, ReplicateTo replicateTo, ExecutorService ec) {
+        this.bucket = currentBucket;
+        this.expirationMillis = expirationMillis;
+        this.persistTo = persistTo;
+        this.replicateTo = replicateTo;
+        this.es = ec;
+        this.ec = ExecutionContext$.MODULE$.fromExecutorService(es);
+        this.couchbase = Couchbase$.MODULE$;
+    }
+
+    public CouchbaseBucket withExecutor(ExecutorService ec) {
+        return new CouchbaseBucket(bucket, expirationMillis, persistTo, replicateTo, ec);
+    }
+
+    public CouchbaseBucket withExpiration(Integer value) {
+        return new CouchbaseBucket(bucket, value, persistTo, replicateTo, es);
+    }
+
+    public CouchbaseBucket withPersistTo(PersistTo value) {
+        return new CouchbaseBucket(bucket, expirationMillis, value, replicateTo, es);
+    }
+
+    public CouchbaseBucket withReplicateTo(ReplicateTo value) {
+        return new CouchbaseBucket(bucket, expirationMillis, persistTo, replicateTo, es);
+    }
+
+    public CouchbaseBucket withClusterOpts(PersistTo value1, ReplicateTo value2) {
+        return new CouchbaseBucket(bucket, expirationMillis, value1, value2, es);
+    }
+
+    public <T> Future<List<T>> find(String docName, String viewName, Query query, Reader<T> reader) {
+        return FutureHelper$.MODULE$.toRCFuture(ScalaHelper$.MODULE$.javaFind(docName, viewName, query, reader, bucket, ec), ec);
+    }
+
+    public <T> Future<List<T>> find(View view, Query query, Reader<T> reader) {
+        return FutureHelper$.MODULE$.toRCFuture(ScalaHelper$.MODULE$.javaFind(view, query, reader, bucket, ec), ec);
+    }
+
+    public <T> Future<List<Row<T>>> search(String docName, String viewName, Query query, Reader<T> reader) {
+        return FutureHelper$.MODULE$.toRCFuture(ScalaHelper$.MODULE$.javaFullFind(docName, viewName, query, reader, bucket, ec), ec);
+    }
+
+    public <T> Future<List<Row<T>>> search(View view, Query query, Reader<T> reader) {
+        return FutureHelper$.MODULE$.toRCFuture(ScalaHelper$.MODULE$.javaFullFind(view, query, reader, bucket, ec), ec);
+    }
+
+    public Future<View> view(String docName, String view) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.javaView(docName, view, bucket, ec), ec);
+    }
+
+    public <T> Future<Functionnal.Option<T>> get(String key, Reader<T> reader) {
+        return FutureHelper$.MODULE$.toRCFuture(ScalaHelper$.MODULE$.javaOptGet(key, reader, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> incr(String key, Integer of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.incr(key, of, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> incr(String key, Long of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.incr(key, of, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> decr(String key, Integer of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.decr(key, of, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> decr(String key, Long of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.decr(key, of, bucket, ec), ec);
+    }
+
+    public Future<Integer> incrAndGet(String key, Integer of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.asJavaInt(couchbase.incrAndGet(key, of, bucket, ec), ec), ec);
+    }
+
+    public Future<Long> incrAndGet(String key, Long of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.asJavaLong(couchbase.incrAndGet(key, of, bucket, ec), ec), ec);
+    }
+
+    public Future<Integer> decrAndGet(String key, Integer of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.asJavaInt(couchbase.decrAndGet(key, of, bucket, ec), ec), ec);
+    }
+
+    public Future<Long> decrAndGet(String key, Long of) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.asJavaLong(couchbase.decrAndGet(key, of, bucket, ec), ec), ec);
+    }
+
+    public <T> Future<OperationStatus> set(String key, T value, Writer<T> fmt) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.javaSet(key, expirationMillis, Json.stringify(fmt.write(value)), persistTo, replicateTo, bucket, ec), ec);
+    }
+
+    public <T> Future<OperationStatus> add(String key, T value, Writer<T> fmt) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.javaAdd(key, expirationMillis, Json.stringify(fmt.write(value)), persistTo, replicateTo, bucket, ec), ec);
+    }
+
+    public <T> Future<OperationStatus> replace(String key, T value, Writer<T> fmt) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.javaReplace(key, expirationMillis, Json.stringify(fmt.write(value)), persistTo, replicateTo, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> delete(String key){
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.delete(key, persistTo, replicateTo, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> flush(int delay) {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.flush(delay, bucket, ec), ec);
+    }
+
+    public Future<OperationStatus> flush() {
+        return FutureHelper$.MODULE$.toRCFuture(couchbase.flush(bucket, ec), ec);
+    }
+}
---
+++ javawrapper/src/main/scala/org/reactivecouchbase/FutureHelper.scala
@@ -0,0 +1,72 @@
+package org.reactivecouchbase
+
+import scala.concurrent.{ExecutionContext, Future}
+import com.couchbase.client.protocol.views._
+import org.reactivecouchbase.client.Row
+import org.reactivecouchbase.json.Reader
+import org.reactivecouchbase.client.CouchbaseFutures._
+import scala.collection.JavaConversions._
+import scala.util.Success
+import scala.util.Failure
+import java.util
+
+object FutureHelper {
+
+  def toRCFuture[T](future: Future[T], ec: ExecutionContext): org.reactivecouchbase.japi.concurrent.Future[T] = {
+    val jpromise = new org.reactivecouchbase.japi.concurrent.Promise[T]()
+    future.onComplete {
+      case Success(result) => jpromise.success(result)
+      case Failure(e) => jpromise.failure(e)
+    }(ec)
+    jpromise.future()
+  }
+}
+
+object ScalaHelper {
+
+  def javaOptGet[T](key: String, reader: Reader[T], bucket: CouchbaseBucket, ec: ExecutionContext): scala.concurrent.Future[org.reactivecouchbase.common.Functionnal.Option[T]] = {
+    waitForGet( bucket.couchbaseClient.asyncGet(key), bucket, ec ).map({
+      case doc: String => reader.read(org.reactivecouchbase.json.Json.parse(doc)) match {
+        case s: org.reactivecouchbase.json.JsSuccess[T] => org.reactivecouchbase.common.Functionnal.Option.some[T](s.get())
+        case _ => org.reactivecouchbase.common.Functionnal.Option.none[T]()
+      }
+      case _ => org.reactivecouchbase.common.Functionnal.Option.none[T]()
+    })(ec).asInstanceOf[scala.concurrent.Future[org.reactivecouchbase.common.Functionnal.Option[T]]]
+  }
+
+  def javaFind[T](docName:String, viewName: String, query: Query, reader: Reader[T], bucket: CouchbaseBucket, ec: ExecutionContext): scala.concurrent.Future[java.util.List[T]] = {
+    Couchbase.view(docName, viewName)(bucket, ec).flatMap { view =>
+      javaFind[T](view, query, reader, bucket, ec)
+    }(ec)
+  }
+
+  def javaFind[T](view: View, query: Query, reader: Reader[T], bucket: CouchbaseBucket, ec: ExecutionContext): scala.concurrent.Future[java.util.List[T]] = {
+    waitForHttp( bucket.couchbaseClient.asyncQuery(view, query), bucket, ec ).map { results =>
+      new util.ArrayList[T](asJavaCollection(results.iterator().collect {
+        case r: ViewRowWithDocs if query.willIncludeDocs() => reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument.asInstanceOf[String]))
+        case r: ViewRowReduced if query.willIncludeDocs() => reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument))
+        case r: SpatialViewRowWithDocs if query.willIncludeDocs() => reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument.asInstanceOf[String]))
+      }.toList.filter(_.isSuccess).map(_.get())))
+    }(ec)
+  }
+
+  def javaFullFind[T](docName:String, viewName: String, query: Query, reader: Reader[T], bucket: CouchbaseBucket, ec: ExecutionContext): scala.concurrent.Future[java.util.List[Row[T]]] = {
+    Couchbase.view(docName, viewName)(bucket, ec).flatMap { view =>
+      javaFullFind[T](view, query, reader, bucket, ec)
+    }(ec)
+  }
+
+  def javaFullFind[T](view: View, query: Query, reader: Reader[T], bucket: CouchbaseBucket, ec: ExecutionContext): scala.concurrent.Future[java.util.List[Row[T]]] = {
+    waitForHttp( bucket.couchbaseClient.asyncQuery(view, query), bucket, ec ).map { results =>
+      new util.ArrayList[Row[T]](asJavaCollection(results.iterator().map {
+        case r: ViewRowWithDocs if query.willIncludeDocs() => new Row[T](Some(reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument.asInstanceOf[String])).get()), r.getId, r.getKey, r.getValue )
+        case r: ViewRowNoDocs => new Row[T](d = None, id = r.getId, key = r.getKey, value = r.getValue )
+        case r: ViewRowReduced if query.willIncludeDocs() => new Row[T](Some(reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument)).get()), "", r.getKey, r.getValue )
+        case r: ViewRowReduced if !query.willIncludeDocs() => new Row[T](d = None, id = "", key = r.getKey, value = r.getValue )
+        case r: SpatialViewRowNoDocs => new Row[T](d = None, id = r.getId, key = r.getKey, value = r.getValue )
+        case r: SpatialViewRowWithDocs if query.willIncludeDocs() => new Row[T](Some(reader.read(org.reactivecouchbase.json.Json.parse(r.getDocument.asInstanceOf[String])).get()), r.getId, r.getKey, r.getValue )
+        case r: SpatialViewRowWithDocs if !query.willIncludeDocs() => new Row[T](d = None, id = r.getId, key = r.getKey, value = r.getValue )
+      }.toList))
+    }(ec)
+  }
+}
